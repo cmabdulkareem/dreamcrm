edcee02d074242f7f5c09ca7b8316df604242776
kareem <cmabdulkareem@gmail.com>
2025-11-09 23:04:04 +0530

    changed to jsvite

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -2,6 +2,7 @@ const corsOptions = {
   origin: ["https://dreamcrm-wjh1.vercel.app", "https://dreamcrm.onrender.com"],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
+  secure: true,
   optionsSuccessStatus: 200,
   allowedHeaders: ["Content-Type", "Authorization"],
 };
