1cad755fc645d9b50a809019e6d2baf3c7618b3b
kareem <cmabdulkareem@gmail.com>
2025-12-10 22:18:24 +0530

    fixed brand specific chat system

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,5 @@
-const API = import.meta.env.VITE_API_URL
-    ? import.meta.env.VITE_API_URL
-    : import.meta.env.PROD
-        ? import.meta.env.VITE_API_URL_PRODUCTION || "https://dreamcrm.onrender.com/api"
-        : "http://localhost:3000/api";
+const API = import.meta.env.PROD
+    ? import.meta.env.VITE_API_URL_PRODUCTION || "https://dreamcrm.onrender.com/api"
+    : import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export default API;
