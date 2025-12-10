034375fa7d5ed5c49628570b4c51d499073e0ea6
kareem <cmabdulkareem@gmail.com>
2025-12-10 21:34:02 +0530

    fixed brand specific chat system

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["https://dreamcrm-nine.vercel.app", "https://dreamcrm.onrender.com", "http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://dreamcrms.vercel.app", "https://dreamcrm.onrender.com", "http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
   optionsSuccessStatus: 200,
