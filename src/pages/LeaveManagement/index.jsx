21dbaff1efd338595960b34fed389523e4a27d40
kareem <cmabdulkareem@gmail.com>
2025-12-17 20:56:08 +0530

    leave management

--- src/pages/LeaveManagement/index.jsx
+++ src/pages/LeaveManagement/index.jsx
@@ -618,7 +618,7 @@ const LeaveManagement = () => {
                 <div className="mb-6">
                   <input
                     type="text"
-                    placeholder="Search by Name"
+                    placeholder="Search by Name or Brand..."
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                     className="w-1/2 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-brand-500 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
