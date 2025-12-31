733e6abddf66b93683892fe294c7fd43920589be
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-01-01 02:30:54 +0530

    announcement fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-4 text-center border-t border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900">
       <p className="text-sm text-gray-500 dark:text-gray-400">
-        &copy; {new Date().getFullYear()} - Dremzone School of Creative Studies | Kasaragod
+       Copyright &copy; {new Date().getFullYear()} - CDC International | Kasaragod
       </p>
     </footer>
   );
