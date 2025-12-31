dd89ce96fb2b0d7a5eadde661a9c1fd1cda0ab7d
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-01-01 02:32:54 +0530

    announcement fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="py-4 text-center border-t border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900">
       <p className="text-sm text-gray-500 dark:text-gray-400">
-       Copyright &copy; {new Date().getFullYear()} - CDC International | Kasaragod
+       Copyright &copy; {new Date().getFullYear()} - CDC International | www.cdcinternational.in
       </p>
     </footer>
   );
