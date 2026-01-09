336244f6311e3f75545e376a069d01f2e8008a08
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-01-09 17:51:31 +0530

    contact points in lead management is now dynamic

--- server/config/corsOptions.js
+++ server/config/corsOptions.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: ["https://dreamcrms.vercel.app", "https://dreamcrm.onrender.com", "http://localhost:5173", "http://localhost:5174"],
+  origin: ["https://dreamcrms.vercel.app", "https://dreamcrm.onrender.com", "https://crm.cdcinternational.in", "http://localhost:5173", "http://localhost:5174"],
   methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
   credentials: true,
   optionsSuccessStatus: 200,
