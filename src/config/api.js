3abf027a05cbfcc869a86b3d61521f6cc447480d
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-01-19 22:54:34 +0530

    removed chat system

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
 const API = import.meta.env.PROD
-    ? import.meta.env.VITE_API_URL_PRODUCTION || "https://dreamcrm.onrender.com/api"
+    ? import.meta.env.VITE_API_URL_PRODUCTION || "/api"
     : import.meta.env.VITE_API_URL || "http://localhost:3000/api";
 
 export default API;
