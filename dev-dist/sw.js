0efcf683be324ecb66c89bbc658988be9c9caba4
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-01-19 21:17:45 +0530

    removed chat system

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kohjmk4asj"
+    "revision": "0.ineau8m30t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
