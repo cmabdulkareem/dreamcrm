5a384182c4fc91c9099a688301dd7fc7c5af62a5
[abdul kareem] <[cmabdulkareem@gmail.com]>
2026-02-20 11:32:29 +0530

    new HR module introduced

--- src/pages/HR/EmployeeList.jsx
+++ src/pages/HR/EmployeeList.jsx
@@ -3,7 +3,7 @@ import PageMeta from '../../components/common/PageMeta';
 import PageBreadCrumb from '../../components/common/PageBreadCrumb';
 import ComponentCard from '../../components/common/ComponentCard';
 import { Link } from 'react-router-dom';
-import { PlusIcon, EditIcon, TrashIcon } from '../../icons';
+import { PlusIcon, PencilIcon, TrashBinIcon } from '../../icons';
 
 const EmployeeList = () => {
     // Placeholder data
@@ -65,10 +65,10 @@ const EmployeeList = () => {
                                     </td>
                                     <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                                         <button className="text-indigo-600 hover:text-indigo-900 dark:text-indigo-400 dark:hover:text-indigo-300 mr-3">
-                                            <EditIcon className="w-5 h-5" />
+                                            <PencilIcon className="w-5 h-5" />
                                         </button>
                                         <button className="text-red-600 hover:text-red-900 dark:text-red-400 dark:hover:text-red-300">
-                                            <TrashIcon className="w-5 h-5" />
+                                            <TrashBinIcon className="w-5 h-5" />
                                         </button>
                                     </td>
                                 </tr>
